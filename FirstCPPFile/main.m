3f60bab9c093931acc2fda822d14f8c558704f33
Ronald Hernandez <2ronald.hernandez@gmail.com>
2015-10-21 04:05:40 -0700

    Initial Commit

---
+++ FirstCPPFile/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FirstCPPFile
+//
+//  Created by Ronald Hernandez on 10/21/15.
+//  Copyright © 2015 Wahoo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
